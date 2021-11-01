7e4c2c23959bb95c637d8f2e7c68961ad9debaaf
steveropa <steve@rmprogrammers.com>
2021-11-01 11:58:01 -0600

    first go at the kata

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -6,7 +6,21 @@ describe('Gilded Rose', function () {
     it('should foo', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].name).to.equal('foo');
     });
+
+    it('should degrade quality twice as fast as normal when item is conjured', function(){
+        const gildedRose = new GildedRose([new Item('conjured', 1,50)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(48);
+    });
+
+    it('should degrade quality by one for normal items', function(){
+        const gildedRose = new GildedRose([new Item('foo', 1,50)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(49);
+    });
+
+
 
 });
